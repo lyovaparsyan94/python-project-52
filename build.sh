fc7e58d15c1039c2ee096b8e49dc4bb7ee182477
Parsyan Lyova <l.parsyan@blask.com>
2025-04-08 14:05:00 +0400

    fix: update localizations

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
+# скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-export PATH=$PATH:/usr/local/python3/bin && \
-pip3 install gunicorn && \
-make install && \
-make translate && \
-make collectstatic && \
-make migrate
+# здесь добавьте все необходимые команды для установки вашего проекта
+# команду установки зависимостей, сборки статики, применения миграций и другие
+export PATH=$PATH:/usr/local/python3/bin && pip3 install gunicorn && make install && make collectstatic && make migrate

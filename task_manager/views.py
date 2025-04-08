1fb0dbd70feea5fd36a52c1f4b47e873a4e8ba20
parsyan.lyova@gmail.com <Lyova Parsyan>
2025-04-08 13:58:15 +0400

    feat: update views

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,9 +1,10 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
-from django.utils.translation import gettext
-from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.messages import info
-
+from django.utils.translation import gettext, activate, get_language
+from django.contrib import messages
+from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 
 class IndexView(TemplateView):
     template_name = "index.html"
@@ -12,6 +13,14 @@ class IndexView(TemplateView):
 class Login(SuccessMessageMixin, LoginView):
     template_name = "login.html"
     success_message = gettext("You are log in")
+
+    def form_valid(self, form):
+        language = self.request.LANGUAGE_CODE
+        activate(language)
+
+        messages.success(self.request, self.success_message)
+
+        return super().form_valid(form)
 
 
 class Logout(LogoutView):

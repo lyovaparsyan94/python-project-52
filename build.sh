89598f0d7ccf71a28fe3cec3bb778b0f54878556
Parsyan Lyova <l.parsyan@blask.com>
2025-04-08 12:53:59 +0400

    fix: compile translations during build

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-export PATH=$PATH:/usr/local/python3/bin && pip3 install gunicorn && make install && make collectstatic && make migrate
+export PATH=$PATH:/usr/local/python3/bin && \
+pip3 install gunicorn && \
+make install && \
+make translate && \
+make collectstatic && \
+make migrate

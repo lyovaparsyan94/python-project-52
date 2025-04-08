65f0f8747436f1a2c9989bf3674e252014154e66
Parsyan Lyova <l.parsyan@blask.com>
2025-04-08 14:03:14 +0400

    fix: update localizations

--- task_manager/templates/users copy/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-
-class User(AbstractUser):
-    
-    def __str__(self):
-        return self.get_full_name()
-
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,10 +1,9 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.messages import info
-from django.utils.translation import gettext, activate, get_language
-from django.contrib import messages
-from django.contrib.auth.views import LoginView
+from django.utils.translation import gettext
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.messages import info
+
 
 class IndexView(TemplateView):
     template_name = "index.html"
@@ -13,14 +12,6 @@ class IndexView(TemplateView):
 class Login(SuccessMessageMixin, LoginView):
     template_name = "login.html"
     success_message = gettext("You are log in")
-
-    def form_valid(self, form):
-        language = self.request.LANGUAGE_CODE
-        activate(language)
-
-        messages.success(self.request, self.success_message)
-
-        return super().form_valid(form)
 
 
 class Logout(LogoutView):

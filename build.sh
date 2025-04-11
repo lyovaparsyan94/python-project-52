2970ea814ec363c66644bfa074107b3eb8b4b637
Parsyan Lyova <l.parsyan@blask.com>
2025-04-12 02:28:35 +0400

    update build file

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+. $HOME/.local/bin/env
 
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
-export PATH=$PATH:/usr/local/python3/bin && pip3 install gunicorn && make install && make collectstatic && make migrate
+make install && make collectstatic && make migrate && make compilemessages

737d3f7641cdddacc23f397e5bc84b1454d43ebd
Parsyan Lyova <l.parsyan@blask.com>
2025-04-13 01:06:26 +0400

    refactor project

--- build.sh
+++ build.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
-set -o errexit
-
+# скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-pip install --upgrade pip
-pip install gunicorn
-
-# ХАК: скопировать gunicorn туда, где Render его ищет
-mkdir -p .venv/bin
-cp $(which gunicorn) .venv/bin/gunicorn
-
-make install
-make collectstatic
-make migrate
-make compilemessages
+# здесь добавьте все необходимые команды для установки вашего проекта
+# команду установки зависимостей, сборки статики, применения миграций и другие
+export PATH=$PATH:/usr/local/python3/bin && pip3 install gunicorn && make install && make collectstatic && make migrate

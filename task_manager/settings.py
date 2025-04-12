a6d8a7b0120d8622b350c0f2cd2b1eb6d87f853b
Parsyan Lyova <l.parsyan@blask.com>
2025-04-13 03:59:52 +0400

    refactor makefile

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -94,15 +94,19 @@ WSGI_APPLICATION = "task_manager.wsgi.application"
 
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    "default": dj_database_url.config(default=os.getenv("DATABASE", "db.sqlite3"))
 }
 
 # Password validation

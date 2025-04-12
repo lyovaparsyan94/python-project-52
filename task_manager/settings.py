c9ce12b0f4d1f450c08b5affe4ccb9bbb2515211
Parsyan Lyova <l.parsyan@blask.com>
2025-04-13 03:51:02 +0400

    refactor makefile

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -93,16 +93,23 @@ WSGI_APPLICATION = "task_manager.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+if os.getenv("GITHUB_ACTIONS") == "true":
+    # Конфигурация для GitHub Actions (например, с SQLite)
+    DATABASES = {
+        "default": dj_database_url.config(default="sqlite:///db.sqlite3")
     }
-}
+else:
+    # Конфигурация для продакшена (PostgreSQL)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

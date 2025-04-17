6e8b719003d09f5d7dbe27a532ecf1162383903d
lyovaparsyan94 <94791803+lyovaparsyan94@users.noreply.github.com>
2025-04-17 21:50:19 +0400

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -136,9 +136,8 @@ LANGUAGE_CODE = os.getenv('LANGUAGE_CODE', 'ru')
 
 TIME_ZONE = "UTC"
 
-USE_I18N = True
-
-USE_L10N = True
+USE_I18N = os.getenv('USE_I18N', True)
+USE_L10N = os.getenv('USE_I18N', True)
 
 USE_TZ = True
 

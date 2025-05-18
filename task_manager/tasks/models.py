2aac94f6c97a788ee50181dc39442f343c2cff59
Parsyan Lyova <l.parsyan@blask.com>
2025-05-19 00:25:36 +0400

    hotfix

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from task_manager.users.models import User
 
 
 class Status(models.Model):

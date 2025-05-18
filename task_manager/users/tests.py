09c2b70b8c57dec3563280f81c9dd0c03e57723c
Parsyan Lyova <l.parsyan@blask.com>
2025-05-19 01:07:05 +0400

    hotfix

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,36 +1,34 @@
-import pytest
+from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
-class TestUser:
-    @pytest.fixture(autouse=True)
-    def setup(self, django_db_setup, django_db_blocker):
-        with django_db_blocker.unblock():
-            User = get_user_model()
-            # Создаем тестовых пользователей
-            User.objects.create_user(
-                username='testuser1',
-                password='testpass123',
-                first_name='Test',
-                last_name='User1',
-                email='testuser1@example.com'
-            )
-            User.objects.create_user(
-                username='testuser2',
-                password='testpass123',
-                first_name='Test',
-                last_name='User2',
-                email='testuser2@example.com'
-            )
-            User.objects.create_user(
-                username='testuser3',
-                password='testpass123',
-                first_name='Test',
-                last_name='User3',
-                email='testuser3@example.com'
-            )
+class UserTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        # Создаем тестовых пользователей
+        User.objects.create_user(
+            username='testuser1',
+            password='testpass123',
+            first_name='Test',
+            last_name='User1',
+            email='testuser1@example.com'
+        )
+        User.objects.create_user(
+            username='testuser2',
+            password='testpass123',
+            first_name='Test',
+            last_name='User2',
+            email='testuser2@example.com'
+        )
+        User.objects.create_user(
+            username='testuser3',
+            password='testpass123',
+            first_name='Test',
+            last_name='User3',
+            email='testuser3@example.com'
+        )
 
-    def test_load_users(self, django_db_blocker, django_user_model):
-        with django_db_blocker.unblock():
-            users = django_user_model.objects.all()
-            assert len(users) == 3 
+    def test_load_users(self):
+        User = get_user_model()
+        users = User.objects.all()
+        self.assertEqual(len(users), 3) 

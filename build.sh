61b670e9f2ddf11d9ab6cbcd2e9430bd8b390763
Parsyan Lyova <l.parsyan@blask.com>
2025-09-05 22:06:49 +0400

    TESTING

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-
+# скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+source $HOME/.local/bin/venv
 
+
+
+# здесь добавьте все необходимые команды для установки вашего проекта
+# команду установки зависимостей, сборки статики, применения миграций и другие
 make install && make collectstatic && make migrate
